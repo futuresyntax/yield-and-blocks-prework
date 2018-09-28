b1a713eb0427dc9f9299890d8ebf3d5fe0e3da8a
FutureScript <fernandobaxter1@gmail.com>
2018-09-28 01:38:34 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -4,15 +4,3 @@ end
 
 # call your method here!
 
-describe "say_hello" do 
-  
-  it 'accepts an argument of a name and prints out Hello with that Name' do
-    expect($stdout).to receive(:puts).with("Hello Kent Beck!")
-    say_hello("Kent Beck")
-  end
-
-  it 'defaults to Ruby Programmer when no name is passed in' do
-    expect($stdout).to receive(:puts).with("Hello Ruby Programmer!")
-    say_hello()
-  end
-end
